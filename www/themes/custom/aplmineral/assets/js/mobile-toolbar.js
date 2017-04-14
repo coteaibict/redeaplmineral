72f267b5a0c396d53e8114fe78363019a81bc755
Gabriel Neutzling <gabrielneutzling@gmail.com>
2017-04-14 12:56:16 -0300

    Adicionando arquivo javascript com funcionalidades do menu fixo para mobile.

---
+++ www/themes/custom/aplmineral/assets/js/mobile-toolbar.js
@@ -0,0 +1,64 @@
+(function ($) {
+  var $html = $('html')
+  var $toolbar = $('.mobile-toolbar')
+  var $btnHome = $toolbar.find('.mobile-toolbar__home')
+  var $btnMenu = $toolbar.find('.mobile-toolbar__menu')
+  var $btnSearch = $toolbar.find('.mobile-toolbar__search')
+
+  var $layers = $toolbar.find('.mobile-toolbar-layer')
+  var $searchLayer = $toolbar.find('.mobile-toolbar-layer--search')
+  var $searchField = $searchLayer.find('input')
+
+
+  // Listeners
+  $btnHome.on('click', handleHomeClick)
+  $btnMenu.on('click', handleMenuClick)
+  $btnSearch.on('click', handleSearchClick)
+
+  // Handler to Home button.
+  function handleHomeClick(event) {
+    if (window.location.pathname === '/') {
+      event.preventDefault()
+      jumpToTop()
+    }
+  }
+
+
+  // Handler to Menu button.
+  function handleMenuClick(event) {
+    event.preventDefault()
+    // @TODO: To be implemented.
+  }
+
+
+  // Handler to Search button.
+  function handleSearchClick(event) {
+    event.preventDefault()
+
+    if ($searchLayer.hasClass('is-visible')) {
+      closeLayer()
+      return
+    }
+
+    closeLayer()
+    $html.addClass('no-scroll')
+    $searchLayer.addClass('is-visible')
+    $searchField.focus()
+  }
+
+
+  // Move to the top of the page.
+  function jumpToTop() {
+    $html.animate({
+      scrollTop: 0
+    }, 600)
+  }
+
+  // Close all layers.
+  function closeLayer() {
+    $html.removeClass('no-scroll')
+    $layers.removeClass('is-visible')
+    $searchField.blur()
+  }
+
+})(jQuery)
