77028f745846f4e74ba53c0caaf1fcaf38bdbcff
Gabriel Neutzling <gabrielneutzling@gmail.com>
2017-05-12 00:02:47 -0300

    Atualização para deixar os scripts de acessibilidade de acordo com o Drupal.

--- www/themes/custom/aplmineral/assets/js/a11y.js
+++ www/themes/custom/aplmineral/assets/js/a11y.js
@@ -1,23 +1,26 @@
+var Drupal = Drupal || {};
 
-(function ($) {
+(function($, Drupal) {
+  Drupal.behaviors.APL_a11y = {
+    attach: function (context, settings) {
+      var $body = jQuery('body')
+      var $btnHighContrast = jQuery('#a11y-high-contrast-button')
+      var isActive = false
+      var cssClasses = 'high-contrast contraste'
 
-  var $body = jQuery('body')
-  var $btnHighContrast = jQuery('#a11y-high-contrast-button')
-  var isActive = false
-  var cssClasses = 'high-contrast contraste'
+      $btnHighContrast.on('click', highContrast)
 
-  $btnHighContrast.on('click', highContrast)
+      function highContrast(event) {
+        event.preventDefault()
 
-  function highContrast(event) {
-    event.preventDefault()
-
-    if (isActive) {
-      isActive = false
-      $body.removeClass(cssClasses)
-    } else {
-      isActive = true
-      $body.addClass(cssClasses)
+        if (isActive) {
+          isActive = false
+          $body.removeClass(cssClasses)
+        } else {
+          isActive = true
+          $body.addClass(cssClasses)
+        }
+      }
     }
   }
-
-})(jQuery)
+})(jQuery, Drupal);
